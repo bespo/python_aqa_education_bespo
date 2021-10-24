063c4e1e06bea1e0cf59b2f70ab4369468046d9c
Alina Bespoiasova <alinabespoiasovaqa@gmail.com>
2021-10-24 20:14:08 +0300

    tests.py added to basics branch

---
+++ Python basics/test10.py
@@ -0,0 +1,9 @@
+def add_numbers(a,b):
+    '''Takes two parameters
+       Returns sum.
+    '''
+    return a + b
+
+print(add_numbers(5,10))
+print(add_numbers.__doc__)
+
---
+++ Python basics/test16.py
@@ -0,0 +1,15 @@
+file = open("111.txt")
+print(file.read())
+
+# for row in file:
+#     print(row)
+
+# s = file.readlines()
+# print(s)
+
+# file = open("111.txt", "w")
+# file.write("feature/basics")
+
+# file = open("111.txt", "a+")
+# file.write("hi\n")
+# file.read()
